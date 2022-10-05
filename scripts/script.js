5ff5c484af1a02eb5c3fc24be7eaab511d81e12f
Thomas Groeneweg <thomasgroeneweg@gmail.com>
2022-10-06 01:15:21 +0200

    Small edits to the HTML, CSS cleanup and JS link

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,29 @@
 console.log("You spooked me!");
 
 // var voor menu knop
-var menuBtn = document.querySelector('button');
+// var menuBtn = document.querySelector('button');
+var button = document.querySelector('button');
+
+// de content van het menu (div)
+// var navToggle = document.querySelector("nav");
+var element = document.querySelector("nav");
 
 // var voor body
 var scrollLock = document.querySelector("body");
 
-// de content van het menu 
-var navToggle = document.querySelector("nav")
 
 
 // execute funtion toggleMenu on menuBtn (header button) click
-menuBtn.addEventListener("click", toggleMenu);
+// menuBtn.addEventListener("click", toggleMenu);
+button.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     // toggle: menuOpen will prevent overflow, disabling scrolling
+    // add .menuOpen to body  
     scrollLock.classList.toggle("menuOpen");
 
     // toggle: menuContent will make the contents of the menu visible.
-    navToggle.classList.toggle("menuContent")
+    // add .menuContent to nav
+    // navToggle.classList.toggle("menuContent");
+    element.classList.toggle("menuContent");
 }
