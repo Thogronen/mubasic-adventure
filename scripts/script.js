3a3762de056b182651046380fb5eeef65e45ef20
Thomas Groeneweg <thomasgroeneweg@gmail.com>
2022-11-05 18:11:55 +0100

    P2 af met filter

--- scripts/script.js
+++ scripts/script.js
@@ -66,3 +66,138 @@ function toggleMenu() {
     menuMoreItemsB.classList.toggle("menuContent");
     spanSwitch.classList.toggle("menuContent");
 }
+
+// Filter
+
+
+var btnVideo = document.querySelector('.btn-video');
+var btnBts = document.querySelector('.btn-bts');
+var btnNews = document.querySelector('.btn-news');
+var btnCp = document.querySelector('.btn-cp');
+var btnArt = document.querySelector('.btn-art');
+var btnInt = document.querySelector('.btn-int');
+var btnHowto = document.querySelector('.btn-howto');
+var btnAll = document.querySelector('.btn-all');
+
+var allItems = document.querySelectorAll('.items');
+
+
+//
+
+function showAll() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+    }
+}
+
+btnAll.addEventListener('click', showAll);
+
+
+// Video
+function showVideoMarketing() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('video')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnVideo.addEventListener('click', showVideoMarketing);
+
+//
+
+// Video
+function showBehindTheScenes() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('bts')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnBts.addEventListener('click', showBehindTheScenes);
+
+// News
+function showNews() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('news')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnNews.addEventListener('click', showNews);
+
+
+// CP
+function showCreativeProcess() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('cp')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnCp.addEventListener('click', showCreativeProcess);
+
+// Art
+function showInpiredByArt() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('art')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnArt.addEventListener('click', showInpiredByArt);
+
+// Int
+function showInterview() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('inter')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnInt.addEventListener('click', showInterview);
+
+// Int
+function showHowto() {
+
+    for (var q = 0; q < allItems.length; q++) {
+        allItems[q].classList.remove('js-hide');
+        //     If the list of items does contain class 'cat-video'...
+        if (!allItems[q].classList.contains('howto')) {
+            //       ... Do NOT hide it, but do hide everything else.
+            allItems[q].classList.add('js-hide');
+        }
+    }
+}
+
+btnHowto.addEventListener('click', showHowto);
